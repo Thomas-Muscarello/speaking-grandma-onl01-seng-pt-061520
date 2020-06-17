46b7de0eca9a1e4c1db9fd480f24b58220443307
Thomas Muscarello <muscarello.thomas@gmail.com>
2020-06-17 02:29:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,8 @@ def speak_to_grandma(phrase)
   if "#{phrase}" == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else if "#{phrase}" == "#{phrase.upcase}"
+    puts "NO, NOT SINCE 1938!"
+  else "#{phrase}" == "#{phrase.upcase}"
     puts 
 
     

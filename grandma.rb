293332bff4fedb2dd3c10a922f53da01b7238a1a
Thomas Muscarello <muscarello.thomas@gmail.com>
2020-06-17 02:45:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 def speak_to_grandma(string)
-  if string != string.upcase
+  if string == string.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif string.upcase
     return "NO, NOT SINCE 1938!"
